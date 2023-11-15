2e0c8a36ed5b382a7078c104fa4ba622afb35119
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-11-15 15:13:29 +0200

    first master commit

---
+++ 8.1 string/8.1 string.cpp
@@ -0,0 +1,44 @@
+﻿// Дано літерний рядок, який містить послідовність символів s[0], ..., s[n], ... 
+// 1. Визначити число входжень в послідовність групи букв “abc”.
+// 2. Замінити кожну групу букв “abc” парою зірочок “ * *”
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
+int Count(const string s, const string cs)
+{
+	size_t pos = 0;
+	int k = 0;
+	while ((pos = s.find(cs, pos)) != -1)
+	{
+		k++;
+		pos++;
+	}
+	return k;
+}
+string Change(string& s)
+{
+ size_t pos = 0;
+ while ((pos = s.find('a', pos)) != -1)
+	 if (s[pos + 1] == 'b' && s[pos+2] == 'c')
+ s.replace(pos, 3, "***");
+ else
+ pos++;
+ return s;
+}
+int main()
+{
+ string str;
+ string cs = "abc";
+ cout << "Enter string:" << endl;
+ getline(cin, str);
+ cout << "String contained " << Count(str, cs) << " groups of 'abc'" << endl;
+ string dest = Change(str);
+ cout << "Modified string (param) : " << str << endl;
+ cout << "Modified string (result): " << dest << endl;
+ return 0;
+}
+
+
+
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include "CppUnitTest.h"
+#include "../8.1 string/8.1 string.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace YourProjectTests
+{
+    TEST_CLASS(YourTestClass)
+    {
+    public:
+
+        TEST_METHOD(TestCountFunction)
+        {
+            const char* testString1 = "abcabcabc";  // тестовий рядок з трьома групами "abc"
+            const char* testString2 = "ababab";     // тестовий рядок без груп "abc"
+            const char* testString3 = "";           // порожній рядок
+
+            const char* cs = "abc";
+
+            // Очікувані результати
+            int expectedCount1 = 3;
+            int expectedCount2 = 0;
+            int expectedCount3 = 0;
+
+            // Перевірка роботи функції Count
+            int result1 = Count(testString1, cs);
+            int result2 = Count(testString2, cs);
+            int result3 = Count(testString3, cs);
+
+            // Перевірка результатів тестів
+            Assert::AreEqual(expectedCount1, result1);
+            Assert::AreEqual(expectedCount2, result2);
+            Assert::AreEqual(expectedCount3, result3);
+        }
+    };
+}
